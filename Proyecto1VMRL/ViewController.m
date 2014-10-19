c82da996fed9eb9ca76ae7dee54093d79de1324f
Victor <aa@iMac-de-a-a.local>
2014-10-19 08:50:39 -0500

    Initial Commit

---
+++ Proyecto1VMRL/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Proyecto1VMRL
+//
+//  Created by Victor on 19/10/14.
+//  Copyright (c) 2014 AMIRD&T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Proyecto1VMRL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Proyecto1VMRL
+//
+//  Created by Victor on 19/10/14.
+//  Copyright (c) 2014 AMIRD&T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
